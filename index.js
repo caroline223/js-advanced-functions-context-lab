88769a5faa5ce4c68b30ef7d8dca12555a803e31
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-04-17 22:45:31 -0400

    commit tree 53f8f88cb4aab728c19be25f1e7bf61adfa11d1d

--- index.js
+++ index.js
@@ -20,3 +20,73 @@ let allWagesFor = function () {
 
     return payable
 }
+
+function createEmployeeRecord(array){
+    let employeeInfo
+    return employeeInfo = {
+       firstName: array[0],
+       familyName: array[1],
+       title: array[2],
+       payPerHour: array[3],
+       timeInEvents: [],
+       timeOutEvents: [],
+    }
+}
+
+function createEmployeeRecords(employee){
+    return employee.map(function(array){
+        return createEmployeeRecord(array)
+    })
+}
+
+function createTimeInEvent(dateStamp){
+
+    let [date, hour] = dateStamp.split(" ")
+    let timeData = {
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    }
+    this.timeInEvents.push(timeData)
+    return this 
+}
+
+function createTimeOutEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(" ")
+    let timeData = {
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    }
+    this.timeOutEvents.push(timeData)
+    return this 
+}
+
+function hoursWorkedOnDate(dateSpecific){
+    let start = this.timeInEvents.find(function(e){
+        return e.date === dateSpecific
+    })
+
+    let end = this.timeOutEvents.find(function(e) {
+        return e.date === dateSpecific
+    })
+
+    return (end.hour - start.hour) / 100 
+}
+
+function wagesEarnedOnDate(dateSpecific){
+    let wages = this.payPerHour * hoursWorkedOnDate.call(this, dateSpecific)
+    return parseFloat(wages.toString())
+}
+
+function findEmployeeByFirstName(srcArray, firstName){
+    return srcArray.find(function(e){
+        return e.firstName === firstName
+    })
+}
+
+function calculatePayroll(arrayOfEmployeeRecords){
+    return arrayOfEmployeeRecords.reduce(function(memo, record){
+        return memo + allWagesFor.call(record)
+    }, 0)
+}
